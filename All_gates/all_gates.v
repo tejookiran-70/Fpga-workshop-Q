05eca2a2eb137738dfdd0e8a4a7f7d32c6dedc27
tejookiran-70 <mekalatejookiran2008@gmail.com>
2026-09-18 19:08:56 +0530

    Create all_gates.v

---
+++ All_gates/all_gates.v
@@ -0,0 +1,16 @@
+module gates(
+input a,
+input b,
+output out_and,
+output out_or,
+output out_xor,
+output out_nand,
+output out_nor,
+output out_xnor);
+assign out_and = a&b;
+assign out_or = a|b;
+assign out_xor = a^b;
+assign out_nand = ~(a&b);
+assign out_nor = ~(a|b);
+assign out_xnor = ~(a^b);
+endmodule
